66e39c5e7a7d18e1cea29f18f1d77928438f8720
yunggeo <zachariasg7@hotmail.gr>
2025-11-27 17:54:26 +0200

    get information dynamic

---
+++ CrudApi/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var blogs = new List<Blog>
+{
+    new Blog {  Title = "First Blog", Content = "This is my first blog post." },
+    new Blog {  Title = "Second Blog", Content = "This is my second blog post." }
+};
+app.MapGet("/", () => "I am root");
+
+app.MapGet("/blogs", () => {return blogs;});
+
+app.MapGet("/blogs/{id}", (int id ) => {
+if (id < 0 || id >= blogs.Count) {
+    return Results.NotFound();
+}
+return Results.Ok(blogs[id]);
+});
+
+app.Run();
+
+public class Blog
+{
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+}
