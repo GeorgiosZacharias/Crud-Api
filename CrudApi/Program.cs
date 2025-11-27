6c14b7fb9875e728d479bc5bab1e1fbe7f87113d
yunggeo <zachariasg7@hotmail.gr>
2025-11-27 18:03:52 +0200

    delete

--- CrudApi/Program.cs
+++ CrudApi/Program.cs
@@ -3,22 +3,38 @@ var app = builder.Build();
 
 var blogs = new List<Blog>
 {
-    new Blog {  Title = "First Blog", Content = "This is my first blog post." },
-    new Blog {  Title = "Second Blog", Content = "This is my second blog post." }
+    new Blog { Title = "First Blog", Content = "This is my first blog post." },
+    new Blog { Title = "Second Blog", Content = "This is my second blog post." }
 };
+
 app.MapGet("/", () => "I am root");
 
-app.MapGet("/blogs", () => {return blogs;});
+app.MapGet("/blogs", () => blogs);
 
-app.MapGet("/blogs/{id}", (int id ) => {
-if (id < 0 || id >= blogs.Count) {
-    return Results.NotFound();
-}
-return Results.Ok(blogs[id]);
+app.MapGet("/blogs/{id}", (int id) =>
+{
+    if (id < 0 || id >= blogs.Count)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(blogs[id]);
 });
 
-app.MapPost("/blogs",(Blog blog) => {blogs.Add(blog);
-return Results.Created($"/blogs/{blogs.Count - 1}", blog);
+app.MapPost("/blogs", (Blog blog) =>
+{
+    blogs.Add(blog);
+    return Results.Created($"/blogs/{blogs.Count - 1}", blog);
+});
+
+app.MapDelete("/blogs/{id}", (int id) =>
+{
+    if (id < 0 || id >= blogs.Count)
+    {
+        return Results.NotFound();
+    }
+    //var blog = blogs[id]; you want to grab it and probably return it
+    blogs.RemoveAt(id);
+    return Results.NoContent();
 });
 
 app.Run();
