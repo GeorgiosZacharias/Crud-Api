c3f9b8d27348937aeb2ee177751a9bfde98d2558
George Zacharias <zachariasg7@hotmail.gr>
2025-11-28 19:01:21 +0200

    MIDDLEWARE LOGGING

--- middleware/Program.cs
+++ middleware/Program.cs
@@ -1,6 +1,12 @@
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(o =>
+{
+
+});
 var app = builder.Build();
 
+app.UseHttpLogging();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
