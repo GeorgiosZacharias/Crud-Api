11de5106e029dff4cfe62b2ce30ea1ce67bc7145
George Zacharias <zachariasg7@hotmail.gr>
2025-11-28 19:15:12 +0200

    custom middleware

--- middleware/Program.cs
+++ middleware/Program.cs
@@ -6,7 +6,15 @@ builder.Services.AddHttpLogging(o =>
 });
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Custom Middleware: Incoming Request");
+    await next.Invoke();
+    Console.WriteLine("Custom Middleware: Outgoing Response");
+});
 app.UseHttpLogging();
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "this is the hello endpoint");
 
 app.Run();
